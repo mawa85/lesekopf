3b26d0d7c43bb3a5c676695c582e3de7d327a771
mawa85 <4971588+mawa85@users.noreply.github.com>
2023-01-17 20:55:39 +0100

    Create d0sendmqtt.sh
    based on the original script here is a script which grabs the needed values and sends them via mqtt to a broker.
    
    I removed the endless loop, but created a cronjob to run this every 2 minutes.
    
    Requirements: mosquitto-pub, which is part of the debian package mosquitto-clients

---
+++ d0sendmqtt.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+stty 300 -F /dev/ttyUSB0 1:0:9a7:0:3:1c:7f:15:4:5:1:0:11:13:1a:0:12:f:17:16:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0
+
+#MQTT server
+ip="192.168.0.10"
+
+#MQTT port
+port="1883"
+
+#MQTT user
+user=""
+
+#MQTT password
+password=""
+
+#MQTT Main Topic
+topic="data/energy"
+
+#MQTT Device ID
+did="SmartMeter"
+
+
+declare -A array
+#Define Datapoints to send via MQTT
+array[Total]="1.8.0("
+array[HT]="1.8.1("
+array[NT]="1.8.2("
+array[W]="1.25("
+array[Imax]="31.25("
+array[Iavg]="51.25("
+array[Imin]="71.25("
+array[Umax]="32.25("
+array[Uavg]="52.25("
+array[Umin]="72.25("
+
+#Processing
+
+
+        ( sleep 1; echo -e "\x2f\x3f\x21\x0d\x0a" > /dev/ttyUSB0 ) &
+        while read -t8 line
+        do
+
+                for i in "${!array[@]}"
+                do
+                         key="${array[$i]}"
+
+                        if [[ $line == "$key"* ]] ; then
+                                key=${line%'('*}
+                                value=${line#*'('}
+                                value=${value%'*'*}
+                                value=${value%')'*}
+                                mosquitto_pub -h $ip -p $port -i $did -u $user -P $password -t "$topic/$key" -m "$value"
+                        fi
+                done
+                #echo $line
+        done < /dev/ttyUSB0
+exit
